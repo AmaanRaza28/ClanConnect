576633db0b27e02538a63cd71db126c144f87f09
Amaan-Raza <amaan.majidi22@gmail.com>
2023-09-11 17:15:20 +0530

    Implemented Authentication and Navigation Sidebar

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface ActionTooltipProps {
+  label: string;
+  align?: "start" | "end" | "center";
+  side?: "top" | "right" | "bottom" | "left";
+  children: React.ReactNode;
+}
+
+export const ActionTooltip = ({
+  label,
+  align,
+  side,
+  children,
+}: ActionTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default ActionTooltip;
